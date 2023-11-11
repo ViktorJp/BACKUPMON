6c93786e64459c8c497cb4693129841c13fe27ac
ViktorJp <97465574+ViktorJp@users.noreply.github.com>
2023-11-11 14:05:19 -0500

    Update and rename backupmon-1.36.sh to backupmon-1.36b1.sh

--- backupmon-1.36b1.sh
+++ backupmon-1.36b1.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Original functional backup script by: @Jeffrey Young, August 9, 2023
-# BACKUPMON v1.36 heavily modified and restore functionality added by @Viktor Jaep, 2023
+# BACKUPMON v1.36b1 heavily modified and restore functionality added by @Viktor Jaep, 2023
 #
 # BACKUPMON is a shell script that provides backup and restore capabilities for your Asus-Merlin firmware router's JFFS and
 # external USB drive environments. By creating a network share off a NAS, server, or other device, BACKUPMON can point to
@@ -16,8 +16,8 @@
 # Please use the 'backupmon.sh -setup' command to configure the necessary parameters that match your environment the best!
 
 # Variable list -- please do not change any of these
-Version="1.36"                                                  # Current version
-Beta=0                                                          # Beta release Y/N
+Version="1.36b1"                                                # Current version
+Beta=1                                                          # Beta release Y/N
 CFGPATH="/jffs/addons/backupmon.d/backupmon.cfg"                # Path to the backupmon config file
 DLVERPATH="/jffs/addons/backupmon.d/version.txt"                # Path to the backupmon version file
 WDAY="$(date +%a)"                                              # Current day # of the week
